1e0b3aea6397380a4324d150057fe09c26fe96c3
ValentinRusseil <valentinrusseil52@gmail.com>
2025-03-07 15:39:15 +0100

    feat(home-page): update footer and add card for 'contenus'

--- Kreativ/views.py
+++ Kreativ/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, template_name="home_page.html", context={})
+    contenus = [
+        {"title": "Titre", "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", "image": "image1.jpg"},
+        {"title": "Titre", "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", "image": None},
+        {"title": "Titre", "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", "image": None},
+    ]
+    return render(request, template_name="home_page.html", context={'contenus': contenus})
