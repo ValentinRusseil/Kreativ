fae0afba68c8543c258fbfd58fe784fc0cf4fcfa
ValentinRusseil <valentinrusseil52@gmail.com>
2025-03-26 20:31:20 +0100

    feat(front): update home page and add new image

--- Kreativ/views.py
+++ Kreativ/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def home_page(request):
     contenus = [
-        {"title": "Titre", "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", "image": "image1.jpg"},
+        {"title": "Titre", "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", "image": "images/wallpaper/fond_contenu.png"},
         {"title": "Titre", "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", "image": None},
         {"title": "Titre", "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", "image": None},
     ]
