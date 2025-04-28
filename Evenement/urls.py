e9709082933bf0c2a89cf24b1a85a996ff5f0528
ValentinRusseil <valentinrusseil52@gmail.com>
2025-04-28 21:33:40 +0200

    feat(Evenement): create Evenement app with models, views, templates, and URL routing
    - Added new Evenement app with essential files including models, views, and templates.
    - Configured URL routing for the Evenement app in the main project.
    - Updated base template to include navigation link to the Evenement page.
    - Added a static image for use in the Evenement templates.

--- Evenement/urls.py
+++ Evenement/urls.py
@@ -14,17 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
-    path('', views.home_page, name="home_page"),
-    path('contact/', include('Contact.urls')),
-    path('a_propos/', include('aPropos.urls')),
-    path('contenu/', include('Contenu.urls')),
-    path('evenement/', include('Evenement.urls')),
+    path('', views.evenement_page, name="evenement_page"),
 ]
